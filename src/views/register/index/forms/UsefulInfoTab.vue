d38b0ddef1730a6921f376bbb1220c1498d411e3
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2020-11-19 10:34:39 +0000

    Added content options list to copy in register and edit

--- src/views/register/index/forms/UsefulInfoTab.vue
+++ src/views/register/index/forms/UsefulInfoTab.vue
@@ -8,6 +8,18 @@
           What do people want to know about your {{ service.type }}? You can add in content for the following options:
         </gov-body>
 
+        <gov-list :bullet="true">
+          <li>Getting here</li>
+          <li>Signing up</li>
+          <li>Meeting up</li>
+          <li>What to wear</li>
+          <li>What to bring</li>
+          <li>How to get here</li>
+          <li>Parking</li>
+          <li>Keeping updated</li>
+          <li>Additional information</li>
+        </gov-list>
+
         <useful-infos-input
           :useful-infos="service.useful_infos"
           @input="$emit('input', { field: 'useful_infos', value: $event })"
